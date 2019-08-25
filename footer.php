25f14ce49e6a0701410691cec5354cbd6c851c8b
kabocha <takaku.genki021@gmail.com>
2019-08-25 20:52:29 +0900

    test

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
     <footer class="footer">
-      <p></p>
+      <p>©︎test site All Right Reserved</p>
     </footer>
     <script src ="js/jquery-2.2.2.min.js"></script>
     <script src="js/main.js"></script>
