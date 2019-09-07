5d99af2803801a06351dca158ca4ae57a30e0503
たかひさ <takaku.genki021@gmail.com>
2019-09-08 01:20:50 +0900

    wrappperクラスを追加

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
     <footer id="js-footer" class="footer">
       <p>Copyright(?) ©︎2019 Sample Site</p>
     </footer>
-  <!-- wrapper???? -->
+  <!-- wrapper??? -->
     <div>
     <script src ="js/jquery-2.2.2.min.js"></script>
   <script src="js/main.js"></script>
