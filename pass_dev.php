f36f42d8c7c0ebb8435fd9f28a93fc71bce27277
髙久 元希 <takaku.genki@LUANDA.local>
2019-10-02 17:37:00 +0900

    開発環境用のパスワードファイルを設定

---
+++ pass_dev.php
@@ -0,0 +1,16 @@
+<?php
+//パスワード管理用
+
+function getDSN($flg){
+    return "mysql:dbname=Portfolio1;host=localhost;charset=utf8";
+
+}
+
+function getDBUser($flg){
+    return('root');
+}
+
+function getDBPass($flg){
+    return('root');
+}
+?>
